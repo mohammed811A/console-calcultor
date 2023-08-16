d63f0a4178ee1876f9a38f5d67a865c4e8498129
mohammed811A <136266848+mohammed811A@users.noreply.github.com>
2023-08-16 09:36:11 +0300

    c#

---
+++ الواجب/الواجب/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Double a, c,b;
+
+//Console.WriteLine("enter the number 1");
+//a = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("enter the sign ");
+//char s = char.Parse(Console.ReadLine());
+
+//Console.WriteLine("enter the number 2");
+//c = Convert.ToInt32(Console.ReadLine());
+
+//switch (s)
+//{
+//    case '+':
+//        b = a + c;
+//        Console.WriteLine($"{a} + {c} ={b}");
+//        break;
+//    case '-':
+//        b = a - c;
+//        Console.WriteLine($"{a} - {c} ={b}");
+//        break;
+//    case '*':
+//        b = a * c;
+//        Console.WriteLine($"{a} * {c} ={b}");
+//        break;
+//    case '/':
+//        b = a / c;
+//        if (c == 0)
+//          Console.WriteLine("erorr");
+//        else
+//          Console.WriteLine($"{a} / {c} ={b}");
+//        break;
+//    default:
+//        Console.WriteLine("tray agen");
+//        break;
+//}
+//Console.ReadKey();
+
+
+
+
+
+
+
